ee2378fea012032502a0a22c266ebe69ca82bbfd
Abdallah El Filali <@elfilali>
2022-09-09 18:10:02 +0200

    supp

---
+++ Java GUI (javax.swing)/Suppliments/RoundedBorder.java
@@ -0,0 +1,17 @@
+package Suppliments;
+
+import javax.swing.JPanel;
+
+public class RoundedBorder extends JPanel {
+    
+    
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
