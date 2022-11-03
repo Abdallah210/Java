ab3365f946a0789d7a8dd868ffb01af3cbbc10d5
Elfilali210 <elfilali210@hotmail.com>
2022-11-03 14:50:41 +0100

    Jbutton

--- Java GUI (javax.swing)/ButtonMain.java
+++ Java GUI (javax.swing)/ButtonMain.java
@@ -1,12 +1,12 @@
-// import secondaryFiles.buttonFrame;  --> for the first frame (buttonFrame.java)
-import secondaryFiles.buttonFrame2;
+import secondaryFiles.buttonFrame;  
+// import secondaryFiles.buttonFrame2;  --> for the second frame (buttonFrame.java)
 
 public class ButtonMain {
 
 
     public static void main(String[] args) {
         
-        new buttonFrame2();
+        new buttonFrame();
 
     }
 
