d20e1635073560b882f6bc6c26d0bea834637157
Elfilali210 <elfilali210@hotmail.com>
2022-11-03 23:52:57 +0100

    start button

--- Games/Chess/Game/interfaces/UserInterface.java
+++ Games/Chess/Game/interfaces/UserInterface.java
@@ -2,9 +2,11 @@ package Game.interfaces;
 
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import Game.Game;
@@ -19,6 +21,8 @@ public class UserInterface extends JLabel {
     private JLabel timeLabel = new JLabel();
     private JLabel player1Label = new JLabel();
     private JLabel player2Label = new JLabel();
+
+    private JButton startButton = new JButton();
 
     private int time;
     private Player player1;
@@ -46,28 +50,49 @@ public class UserInterface extends JLabel {
         this.mainLabel.setOpaque(true);
 
         // Time label :
-        this.timeLabel.setBounds(562,350,475,500);
+        this.timeLabel.setBounds(562,350,475,400);
         this.timeLabel.setBorder(BorderFactory.createLineBorder(new Color(0x272b2e), 3));
         this.timeLabel.setBackground(new Color(0x455565));
         this.timeLabel.setOpaque(true);
 
         // Player labels :
-        this.player1Label.setBounds(112,350,425,500);
+        this.player1Label.setBounds(112,350,425,400);
         this.player1Label.setBorder(BorderFactory.createLineBorder(new Color(0x272b2e), 3));
         this.player1Label.setBackground(new Color(0x455565));
         this.player1Label.setOpaque(true);
 
-        this.player2Label.setBounds(1062,350,425,500);
+        this.player2Label.setBounds(1062,350,425,400);
         this.player2Label.setBorder(BorderFactory.createLineBorder(new Color(0x272b2e), 3));
         this.player2Label.setBackground(new Color(0x455565));
         this.player2Label.setOpaque(true);
 
+        // Start label :
+        this.startButton.setText("Start"); 
+        this.startButton.setBounds((this.getWidth()-175)/2,800,175,100);
+        this.startButton.setFocusable(false);
+        this.startButton.setHorizontalTextPosition(JButton.CENTER);
+        this.startButton.setVerticalTextPosition(JButton.TOP);
+        this.startButton.setFont(new Font("Comic Sans",Font.BOLD, 20));
+        this.startButton.setBackground(new Color(0x616e7c));
+        this.startButton.setForeground(new Color(0xc7ccd1));
+        this.startButton.setEnabled(true);
 
-        this.add(logoLabel);
-        this.add(timeLabel);
-        this.add(player1Label);
-        this.add(player2Label);
-        this.add(mainLabel);
+        this.add(startButton);
+
+        JLabel[] labels = {logoLabel, timeLabel, player1Label, player2Label, mainLabel};
+        
+        for (JLabel label : labels) {
+            this.add(label);
+        }
+
+        // pass to game interface :
+        startButton.addActionListener(     
+            e -> {
+                this.game.getLandingInterface().setVisible(false);
+                this.game.getUserInterface().setVisible(false);
+                this.game.getGameInterface().setVisible(true);
+                }
+            );   
     }
 
     public Player getPlayer1() {
